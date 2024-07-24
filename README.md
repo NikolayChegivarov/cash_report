a25f959b0fcf39f31c34d6b79f67443c87368daa
Nikolai Gusarov <nikolai_polos@mail.ru>
2024-07-24 15:03:01 +0300

    start2

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
+
 Ресурс предназначен для кассовой отчетности. 
+
+pip install django  ?  
+
+pip install djangorestframework  
+
+pip install psycopg2-binary  
+
+pip install python-dotenv  # переменные окружения
+
+python manage.py makemigrations
+
+
--- multiplayer_mode/settings.py
+++ multiplayer_mode/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
-from config import password
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,11 +83,11 @@ WSGI_APPLICATION = 'multiplayer_mode.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'lombard',
+        'NAME': os.getenv('NAME_BD'),
         'HOST': 'localhost',
         'PORT': '5432',
         'USER': 'postgres',
-        'PASSWORD': password,
+        'PASSWORD': os.getenv('PASSWORD'),
         'OPTIONS': {
             'client_encoding': 'UTF8',
             'options': '-c search_path=lombards'

59d63317d772dc5acbf8fe316f7ea23e4e720bd0
Nikolai Gusarov <nikolai_polos@mail.ru>
2024-10-25 12:26:11 +0300

    criate settings.py3

--- multiplayer_mode/settings.py
+++ multiplayer_mode/settings.py
@@ -89,8 +89,8 @@ DATABASES = {
         "PORT": "5432",
         "USER": os.getenv("USER_POSTGRES"),
         "PASSWORD": os.getenv("PASSWORD"),
-        "OPTIONS": {"client_encoding": "UTF8", "options": "-c search_path=lombards"},
-        "SCHEMA": "lombards"
+        # "OPTIONS": {"client_encoding": "UTF8", "options": "-c search_path=lombards"},
+        # "SCHEMA": "lombards",
     }
 }
 
